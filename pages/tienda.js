6b5fa377e6f92f3e41432c8bd292036089b5d693
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2023-01-18 18:36:26 +0100

    add filter

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,5 +1,5 @@
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
 import Layout from '../components/Layout'
 import Listado from '../components/Listado'
 import Spinner from '../components/Spinner'
@@ -7,41 +7,59 @@ import styles from '../styles/Tienda.module.css'
 
 const Tienda = ({ carrito }) => {
  const [guitarras, setGuitarras]=useState([])
- const [ordenar, setOrdenar]=useState('_sort=nombre')
+ 
  const [cargando, setCargando]=useState(false)
  
- const ordenarPorPrecio = (valor)=>{
-// valores del select para pedir a la API con el orden deseado
-switch(valor){
-  case 'mayorAMenor':
-    setOrdenar('_sort=precio:desc')
-    break;
-  case 'menorAMayor':
-    setOrdenar('_sort=precio:ASC') 
-    break;
-  case 'nombreAz':
-    setOrdenar('_sort=nombre')
-    break;   
-  case 'nombreZa':
-    setOrdenar('_sort=nombre:desc')
-    break;  
-  }
-    
-}
 
- useEffect(()=>{
-   setCargando(true)
-    const obtenerDatos = async ()=>{
-    const urlGuitar = `${process.env.NEXT_PUBLIC_API_URL}/guitarras?${ordenar}` // aplicamos el orden en la url con el operador de strapi
-    const respuesta = await fetch(urlGuitar)
-    const guitarrasApi = await respuesta.json()
-    setGuitarras(guitarrasApi)
-    setCargando(false)
-   }
-   
-   obtenerDatos()
-   
- },[ordenar])   // cada vez que cambia el state de ordenar se vuelve a renderizar la pagina ordenada
+ const ordenarPorPrecio = ( valor )=>{
+    const guitarrasOrdenadas = []
+    setCargando(true)
+    switch(valor){
+            
+        case 'mayorAMenor':
+               guitarrasOrdenadas = guitarras.sort((a,b)=> b.precio - a.precio);
+               
+          break;
+        case 'menorAMayor':
+               guitarrasOrdenadas = guitarras.sort((a,b)=> a.precio - b.precio); 
+               
+
+          break;
+        case 'nombreAz':
+               guitarrasOrdenadas =guitarras.sort((a,b)=> a.nombre.localeCompare( b.nombre )); 
+               
+
+          break;   
+        case 'nombreZa':
+               guitarrasOrdenadas = guitarras.sort((a,b)=> b.nombre.localeCompare( a.nombre ));
+           
+
+          break; 
+
+        
+      } 
+      setGuitarras( guitarrasOrdenadas )  
+      setTimeout(() => {
+        setCargando(false)
+      }, 200);
+      
+    };
+
+  useEffect(()=>{
+      setCargando(true)
+       obtenerDatos()
+       
+     },[]);
+
+  const obtenerDatos = async ()=>{
+      const urlGuitar = `${process.env.NEXT_PUBLIC_API_URL}/guitarras?` 
+      const respuesta = await fetch(urlGuitar)
+      const guitarrasApi = await respuesta.json()
+      setGuitarras(guitarrasApi)
+      setCargando(false)
+    }
+
+
  
   
   return (
@@ -53,14 +71,14 @@ switch(valor){
    {cargando ? <Spinner/> : 
       <main className='contenedor'>
            <h1 className='heading'>Nuestra Colección</h1>
-           <select className={styles.select} onChange={e=>ordenarPorPrecio(e.target.value)}>
+           <select className={styles.select} onChange={e => ordenarPorPrecio(e.target.value)}>
              <option value=''>Ordenar por-</option>
              <option value='mayorAMenor'>Precio de mayor a menor</option>
              <option value='menorAMayor'>Precio de menor  a mayor</option>
              <option value='nombreAz'>Nombre A a Z</option>
              <option value='nombreZa'>Nombre Z a A</option>
            </select>
-           <Listado guitarras = {guitarras}/>
+           <Listado guitarras = { guitarras }/>
       </main>} 
 
     
